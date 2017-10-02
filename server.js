6c92956b845e0823224c24f15c4612beacdcb0fa
Samartha K V <samarthakv29@gmail.com>
2017-10-02 17:58:20 -0400

    Updated express-mongodb dependency

--- server.js
+++ server.js
@@ -1,31 +1,64 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-// API file for interacting with MongoDB
-const api = require('./server/routes/api');
+var CONTACTS_COLLECTION = "users";
 
-// Parsers
+var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
-// Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-// API location
-app.use('/api', api);
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-//Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
 
-const server = http.createServer(app);
+///API code
 
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+  
+  /*  "/api/contacts"
+   *    GET: finds all contacts
+   *    POST: creates a new contact
+   */
+  
+  app.get("/api/users", function(req, res) {
+  });
+  
+  app.post("/api/users", function(req, res) {
+  });
+  
+  /*  "/api/contacts/:id"
+   *    GET: find contact by id
+   *    PUT: update contact by id
+   *    DELETE: deletes contact by id
+   */
+  
+  app.get("/api/users/:id", function(req, res) {
+  });
+  
+  app.put("/api/users/:id", function(req, res) {
+  });
+  
+  app.delete("/api/users/:id", function(req, res) {
+  });
