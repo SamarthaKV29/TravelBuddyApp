71b7641786489ed4cf46f440ec906e868447d8c6
ashamra <asharma>
2017-10-10 20:36:12 -0400

    up test

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../../_services/user.service';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  selector: 'user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css'],
+  providers: [ UserService ]
 })
-export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+export class UserDetailComponent implements OnInit{
+  
+  profileData: Object;
 
-  ngOnInit() {
+  constructor (private UserService: UserService) {
+  	profileData = {
+  		
+  	};
+  }
+
+  ngOnInit(){
+    this.UserService
+    .getUsers()
+    .then((Users: User[]) => {
+      this.users = Users.map((User) => {
+        return User;
+      });
+      this.userslen = this.users.length;
+    });
+    
+  }
+  
+  
+  createUser(user: User) {
+    if(this.user._id === ""){
+      this.user._id = (this.userslen + 1).toString();
+      console.log(this.user);
+    }
+    this.UserService.createUser(user);
   }
 
 }
