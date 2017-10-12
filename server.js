c3b872ce5a71059b0a465cafda79917f86ac84dc
Samartha K V <samarthakv29@gmail.com>
2017-10-12 04:25:01 -0400

    CHANGES server.js

--- server.js
+++ server.js
@@ -1,109 +1,166 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+//var mongodb = require("mongodb");
+var mongoose = require("mongoose");
+var methodOverride = require('method-override');
+//var ObjectID = mongodb.ObjectID;
+
+
 var HOMEPAGE = "index";
 var USER_COLLECTION = "users";
+
 
 
 if (process.env.MONGODB_URI == undefined) {
   process.env.MONGODB_URI = "mongodb://heroku_tdjrgd33:k3j5qi89b97t5lr0jo2arb7umt@ds147274.mlab.com:47274/heroku_tdjrgd33";
 }
 
-var app = express();
-app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URI, {
+  server: {
+     reconnectTries: Number.MIN_VALUE
+  },
+  useMongoClient: true
+});
 
+var app = express();
 var distDir = __dirname + "/dist";
 app.set('views', distDir);
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static(path.join(__dirname, 'dist', 'src')));
+app.use(express.static(path.join(distDir, 'src')));
 // app.use(express.static(path.join(__dirname, 'dist', 'src', 'app')));
 app.use(express.static(distDir));
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+app.listen(4500, ()=>{
+  console.log("App started");
+});
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  username: String,
+  created: {
+      date: Date
+  },
+  roles: Array,
+  password: String,
+  email: String,
+  phone: String,
+  name: String,
+  profileData: Object
+});
+
+var User = mongoose.model('User', UserSchema);
+
+
+
+
 app.use((req, res, next) => {
   res.header("Content-Type", 'application/json');
   next();
 });
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 4500, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+
+app.use(function(req,res,next){
+  req.db = db;
+  next();
 });
-
 
 ///API code
 
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({ "error": message });
-}
-goHome = (resp) => {
-  resp.status(200).sendFile(path.join(distDir + '/index.html'));
-}
 
-app.get(["/"], (req, res) => {
-  goHome(res);
-});
+// function handleError(res, reason, message, code) {
+//   console.log("ERROR: " + reason);
+//   res.status(code || 500).json({ "error": message });
+// }
+// goHome = (resp) => {
+//   resp.status(200).sendFile(path.join(distDir + '/index.html'));
+// }
+
 
 app.get("/api/users", function (req, res) {
-  db.collection(USER_COLLECTION).find({}).toArray(function (err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get user details.");
-    } else {
-      res.status(200).json(docs);
-    }
+  User.find((err, users) => {
+    if(err)
+      res.send(err);
+    res.json(todos);
   });
 });
 
 app.post("/api/users", function (req, res) {
-  db.collection(USER_COLLECTION).insertOne(newUser, function (err, doc) {
-    if (err) {
-      return res.redirect("/signup");
-    } else {
-      return res.status(201).json(doc.ops[0]);
-    }
+  User.create({
+    text : req.body.text,
+    done : false
+  }, (err, user) => {
+    if(err)
+      res.send(err);
+    
+      User.find((err, users) =>{
+        if(err)
+          res.send(err);
+        res.json(users);
+      })
   });
 });
 
-
-app.get("/home/:username", function (req, res) {
-  db.collection(USER_COLLECTION).find({ username: req.body.username }).toArray(function (err, docs) {
-    console.log("USERNAME" + docs[0]);
-  });
-});
 
 app.put("/api/users/:username", function (req, res) {
+
 });
 
 app.delete("/api/users/:username", function (req, res) {
+
 });
+
+
+app.get('*', (req, res)=> {
+  res.sendFile('./dist/index.html');
+});
+
+
+
+
+
+
+
 
 //ERROR handling
 
-app.get('*', function (req, res, next) {
-  goHome();
-});
+// app.get('*', function (req, res, next) {
+//   goHome();
+// });
 
-app.use(function (err, req, res, next) {
-  if (err.status !== 404) {
-    return next();
-  }
-  goHome(res);
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: err
+      });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+      message: err.message,
+      error: {}
+  });
 });
