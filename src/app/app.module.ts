e1f51b6c8cfa8142dc9ec95230cc7f887ec1829f
Samartha K V <samarthakv29@gmail.com>
2017-12-05 18:19:11 -0500

    Upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { AppComponent } from './app.component';
-//import { UserDetailComponent } from './users/user-detail/user-detail.component';
-//import { UserService } from './_services/user.service';
+//import { UserDetailComponent } from './users/user-detai
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 //import { UserProfileComponent } from './users/user-profile/user-profile.component';
-//import { ForgotPassComponent } from './users/forgot-pass/forgot-pass.component';
+//import { ForgotPassComponent } from './users/fl/user-detail.component';
+//import { UserService } from './_services/user.service';orgot-pass/forgot-pass.component';
 import { ErrorComponent } from './utilities/error/error.component';
 import { LandingComponent } from './utilities/landing/landing.component';
 import { DateTimePickerModule } from 'ng-pick-datetime';
 import { AboutComponent } from './utilities/about/about.component';
 import { TeamComponent } from './utilities/team/team.component';
+import { SocialLoginModule } from "angular4-social-login";
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
 
 
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("230271184229-ipk8nc2c9r1bfiuvpn0naa5a69ggnojk.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("227631451109453")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 const homeRoutes: Routes = [
   // { path: "create", component: CreatetripComponent},
   // { path: "manage", component: ManagetripsComponent},
@@ -26,9 +42,8 @@ const homeRoutes: Routes = [
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent, children: homeRoutes, pathMatch: "full"},
   //{ path: 'signup/:regstate', component: UserDetailComponent },
-  { path: 'signup', redirectTo: 'signup/new', pathMatch: "full"},
-  { path: 'login', redirectTo: 'login/true', pathMatch: "full"},
-  { path: 'login/:loginstate', component: LoginComponent, pathMatch: "full"},
+  { path: 'signup', redirectTo: 'login', pathMatch: "full"},
+  { path: 'login', component: LoginComponent, pathMatch: "full"},
   //{ path: 'forgotpass', component: ForgotPassComponent },
   //{ path: 'profile/:user', component: UserProfileComponent},
   //{ path: 'profile/:user/:updstate', component: UserProfileComponent },
@@ -55,9 +70,14 @@ const appRoutes: Routes = [
     TeamComponent
   ],
   imports: [
-    BrowserModule, DateTimePickerModule, BrowserAnimationsModule, ReactiveFormsModule, FormsModule, HttpModule, RouterModule.forRoot( appRoutes)
+    BrowserModule, SocialLoginModule, DateTimePickerModule, BrowserAnimationsModule, ReactiveFormsModule, FormsModule, HttpModule, RouterModule.forRoot( appRoutes)
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent],
   exports: [RouterModule]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,43 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'angular4-social-login';
+import { SocialUser } from 'angular4-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
 
 @Component({
   selector: 'social-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  //user: SocialUser;
+  user: SocialUser;
   loggedIn: boolean;
-  // constructor(private authService: AuthService) { 
-  //   setInterval(()=>{
-  //     if(this.loggedIn && this.user){
-  //       console.log("logged In : " + this.user.name);
-  //     }
-  //   }, 2000);
-  // }
-
-  // ngOnInit() {
-  //   this.authService.authState.subscribe((user) => {
-  //     this.user = user;
-  //     this.loggedIn = (user != null);
-  //   });
-  // }
-
-  // signInWithGoogle(): void {
-  //   this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then()
-  // }
-
-  // signInWithFB(): void {
-  //   this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(resp=>{
-  //     this.user = resp;
-  //     console.log("USER");
-  //   }, rej=>rej);
-  // }
-
-  // signOut(): void {
-  //   this.authService.signOut();
-  // }
-
+  constructor(private authService: AuthService) { }
+  
+    ngOnInit() {
+      this.authService.authState.subscribe((user) => {
+        this.user = user;
+      });
+    }
+  
+    signInWithGoogle(): void {
+      this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    }
+  
+    signInWithFB(): void {
+      this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    }
+  
+    signOut(): void {
+      this.authService.signOut();
+    }
 }
