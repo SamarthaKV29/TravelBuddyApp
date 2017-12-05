5d0eae73e0a6eb8e3f8e03cdb690d85e8ee98307
Samartha K V <samarthakv29@gmail.com>
2017-12-04 20:39:25 -0500

    Upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2} from '@angular/core';
+import { RouterLink, Router, ActivatedRoute, Event as RouterEvent, NavigationStart, NavigationCancel, NavigationEnd, NavigationError} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  landing: boolean = true;
+  isLoggedin: boolean = false;
+  title = 'TravelBuddy';  
+  collapsed: boolean = false;
+  loading: boolean = true;
+
+  @ViewChild('collapsible') collapsible: ElementRef;
+  
+  constructor(private router: Router, private route: ActivatedRoute, private renderer: Renderer2){
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.navIntercept(event);
+    });
+    // sessionStorage.clear();
+    // let checker = setInterval(()=>{
+    //   //console.log("loginstate: " + this.isLoggedin, "sessionStore: " + sessionStorage.getItem('token'));
+    //   if(sessionStorage.getItem('token')){
+    //     this.isLoggedin = true;
+    //     var tok = sessionStorage.getItem('token');
+    //     var s = JSON.parse(tok);
+    //     this.currentUser = s.user;
+    //   }
+    //   else if(!sessionStorage.getItem('token')){
+    //     this.isLoggedin = false;
+    //     this.currentUser = null;
+    //   }
+    // }, 200);
+    
+  }
+
+  navIntercept(event: RouterEvent): void{
+    if(event instanceof NavigationStart){
+      this.loading = true;
+    }
+    if(event instanceof NavigationEnd){
+      this.loading = false;
+    }
+    if(event instanceof NavigationCancel || event instanceof NavigationError){
+      this.loading = false;
+    }
+  }
+  ngOnInit(){
+    //console.log(this.router.url);
+    setInterval(()=>{
+      if(this.router.url == "/"){
+        this.landing = true;
+      }
+      else{
+        this.landing = false;
+      }
+    }, 200)
+  }
+
+  collapse(){
+    if(this.collapsed){
+      this.renderer.removeClass(this.collapsible.nativeElement, 'collapse');
+      this.collapsed = false;
+      return;
+    }
+    else{
+      this.renderer.addClass(this.collapsible.nativeElement, 'collapse');
+      this.collapsed = true;
+      return;
+    }
+        
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,38 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { RouterModule, Routes } from '@angular/router';
 import { SocialLoginModule } from "angular4-social-login";
 import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './utilities/landing/landing.component';
+import { ErrorComponent } from './utilities/error/error.component';
+import { AboutComponent } from './utilities/about/about.component';
+import { TeamComponent } from './utilities/team/team.component';
+import { DateTimePickerModule } from 'ng-pick-datetime';
+
+import { InboxComponent } from './inbox/inbox.component';
+import { ManagetripsComponent } from './managetrips/managetrips.component';
+import { CreatetripComponent } from './createtrip/createtrip.component';
+
+const homeRoutes: Routes = [
+  { path: "create", component: CreatetripComponent},
+  { path: "manage", component: ManagetripsComponent},
+  { path: "inbox", component: InboxComponent}
+];
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent, children: homeRoutes},
+  //{ path: 'signup/:regstate', component: UserDetailComponent },
+  { path: 'signup', redirectTo: 'signup/new'},
+  { path: 'login', redirectTo: 'login/true'},
+  { path: 'login/:loginstate', component: LoginComponent},
+  //{ path: 'forgotpass', component: ForgotPassComponent },
+  //{ path: 'profile/:user', component: UserProfileComponent},
+  //{ path: 'profile/:user/:updstate', component: UserProfileComponent },
+  { path: 'error', component: ErrorComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'team', component: TeamComponent}
+];
 
 let config = new AuthServiceConfig([
   {
@@ -24,16 +53,27 @@ export function provideConfig() {
   return config;
 }
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    LandingComponent,
+    ErrorComponent,
+    AboutComponent,
+    TeamComponent,
+    InboxComponent,
+    ManagetripsComponent,
+    CreatetripComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    SocialLoginModule.initialize(provideConfig())
+    SocialLoginModule.initialize(provideConfig()),
+    RouterModule.forRoot( appRoutes )
   ],
   providers: [
     {
@@ -41,6 +81,7 @@ export function provideConfig() {
       useFactory: provideConfig
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
