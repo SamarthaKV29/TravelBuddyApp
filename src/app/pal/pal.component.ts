c49439c1a54b2ac707906cda3f19559f5bd41ab6
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-03 04:08:59 -0400

    Upd

--- src/app/pal/pal.component.ts
+++ src/app/pal/pal.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {PalService} from './pal.service';
-import {Pal} from './pal';
-import {SocialUser} from 'angular4-social-login';
+import { PalService } from './pal.service';
+import { Pal } from './pal';
+import { SocialUser } from 'angular4-social-login';
+import { TripService } from '../trip/trip.service';
+import { Trip } from '../trip/trip';
 
 
 
@@ -14,11 +16,23 @@ export class PalComponent implements OnInit {
 
 
   currUser: SocialUser;
-  
-  constructor() { }
+  trips: Trip[];
+  pals: Pal[] = [
+    new Pal("snehakou@gmail.com", [], "req"),
+  ]
+  constructor(private tripservice: TripService) {
+    this.currUser = JSON.parse(localStorage.getItem("UserTok"));
+    if (!this.trips && this.currUser != null)
+      this.tripservice.getTrips(this.currUser.email).then((trips: Trip[]) => {
+        this.trips = trips.map((trip) => {
+          return trip;
+        });
+      });
+  }
 
   ngOnInit() {
 
   }
+
 
 }
